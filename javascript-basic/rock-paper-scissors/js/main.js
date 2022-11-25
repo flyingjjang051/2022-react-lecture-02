714455d7fcedc16e540bfe53f004e9d46297c665
jjang051 <flyjjang051@gmail.com>
2022-11-25 14:00:25 +0900

    end

--- javascript-basic/rock-paper-scissors/js/main.js
+++ javascript-basic/rock-paper-scissors/js/main.js
@@ -39,24 +39,45 @@ const comList = document.querySelectorAll("#com ul li");
 const playerList = document.querySelectorAll("#player ul li");
 const result = document.querySelector("#result ul");
 const cover = document.querySelector("#cover");
+const resultTxt = cover.querySelector("h1 strong");
+const btnRestart = cover.querySelector("#btnRestart");
+//resultTxt.textContent = "bla~~bla~~bla";
 // cover.classList.add("on");
 let random = 0;
 let gameCount = 0;
+let clearComChoice = 0;
+let clearReset = 0;
+let winCount = 0;
+let drawCount = 0;
+let loseCount = 0;
 //pseudo array
 //think aloud
 
 function playerChoice() {
   playerList.forEach(function (item, idx) {
     item.addEventListener("click", function () {
+      gameCount++;
+
       document.body.classList.add("blocking");
       clearInterval(clearComChoice);
       const selectedId = parseInt(this.dataset.id);
       decide(selectedId, random);
-      setTimeout(function () {
-        console.log("나는 바로 실행되지 않고 1초뒤에 한번만 실행됩니다.");
-        clearComChoice = setInterval(comChoice, 20);
-        document.body.classList.remove("blocking");
-      }, 1000);
+      restartFunc();
+      if (gameCount >= 3) {
+        cover.classList.add("on");
+        clearInterval(clearComChoice);
+        clearTimeout(clearReset);
+        // 조건 달아보기.....
+        // ddd=3, w=1&&d=1&&l=1  비겼을때
+        // w>=2, w=1 d==2  이겼을때
+        if (drawCount === 3 || (winCount >= 1 && drawCount >= 1 && loseCount >= 1)) {
+          resultTxt.textContent = "DRAW";
+        } else if (winCount >= 2 || (winCount >= 1 && drawCount >= 2)) {
+          resultTxt.textContent = "WIN";
+        } else {
+          resultTxt.textContent = "DRAW";
+        }
+      }
       // 리팩토링....
     });
   });
@@ -69,30 +90,21 @@ function decide(playerNum, comNum) {
     console.log("draw");
     li.textContent = "D";
     li.classList.add("draw");
+    drawCount++;
     //result.innerHTML += `<li class="draw">D</li>`;
     //result.append(`<li class="draw">D</li>`);
-  } else if (comNum === 0 && playerNum === 1) {
+  } else if ((comNum === 0 && playerNum === 1) || (comNum === 1 && playerNum === 2) || (comNum === 2 && playerNum === 0)) {
     console.log("win");
     li.textContent = "W";
     li.classList.add("win");
-    //result.innerHTML += `<li class="win">W</li>`;
-    //result.append(`<li class="win">W</li>`);
-  } else if (comNum === 1 && playerNum === 2) {
-    console.log("win");
-    li.textContent = "W";
-    li.classList.add("win");
-    //result.innerHTML += `<li class="win">W</li>`;
-    //result.append(`<li class="win">W</li>`);
-  } else if (comNum === 2 && playerNum === 0) {
-    console.log("win");
-    li.textContent = "W";
-    li.classList.add("win");
+    winCount++;
     //result.innerHTML += `<li class="win">W</li>`;
     //result.append(`<li class="win">W</li>`);
   } else {
     console.log("lose");
     li.textContent = "L";
     li.classList.add("lose");
+    loseCount++;
     //result.innerHTML += `<li class="lose">L</li>`;
     //result.append(`<li class="lose">L</li>`);
   }
@@ -112,10 +124,21 @@ function comChoice() {
     //idx === random ? (item.style.display = "block") : (item.style.display = "none");
   });
 }
-//console.log("01");
-//console.log("02");
-//comChoice();
-let clearComChoice = setInterval(comChoice, 20); // 자바스크립트는 비동기적으로 동작한다.
+function restart() {
+  cover.classList.remove("on");
+  gameCount = 0;
+  winCount = 0;
+  drawCount = 0;
+  loseCount = 0;
+  result.innerHTML = "";
+  restartFunc();
+}
+function restartFunc() {
+  clearReset = setTimeout(function () {
+    clearComChoice = setInterval(comChoice, 20);
+    document.body.classList.remove("blocking");
+  }, 1000);
+}
+btnRestart.addEventListener("click", restart);
 playerChoice();
-
-//clearInterval(clearComChoice);
+restart();
