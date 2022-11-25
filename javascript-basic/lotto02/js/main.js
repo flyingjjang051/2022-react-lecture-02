2f7e6a863df91c04daff2c5d35b45ffb43db63ac
jjang051 <flyjjang051@gmail.com>
2022-11-25 17:13:21 +0900

    end

--- javascript-basic/lotto02/js/main.js
+++ javascript-basic/lotto02/js/main.js
@@ -1,44 +1,58 @@
 const result = document.querySelector("#result");
-const ul = document.createElement("ul");
-result.appendChild(ul);
-
-const nums = Array(45)
-  .fill(10)
-  .map(function (current, idx) {
-    return idx + 1;
-  });
-const selectedNums = [];
-for (let i = 0; i < 6; i++) {
-  const selected = Math.floor(Math.random() * nums.length);
-  selectedNums.push(nums.splice(selected, 1).pop());
-}
-selectedNums.sort(compare);
-selectedNums.forEach(function (item, idx) {
-  const li = document.createElement("li");
-  //li.classList.add("yellow");
-  li.textContent = item;
-
-  if (item >= 1 && item <= 10) {
-    li.classList.add("yellow");
-  } else if (item >= 11 && item <= 20) {
-    li.classList.add("blue");
-  } else if (item >= 21 && item <= 30) {
-    li.classList.add("red");
-  } else if (item >= 31 && item <= 40) {
-    li.classList.add("gray");
-  } else {
-    li.classList.add("green");
+const radios = document.querySelectorAll(".radio");
+const colors = ["yellow", "blue", "red", "gray", "green"];
+function makeLotto() {
+  const ul = document.createElement("ul");
+  result.appendChild(ul);
+  const nums = Array(45)
+    .fill(10)
+    .map(function (current, idx) {
+      return idx + 1;
+    });
+  const selectedNums = [];
+  for (let i = 0; i < 6; i++) {
+    const selected = Math.floor(Math.random() * nums.length);
+    selectedNums.push(nums.splice(selected, 1).pop());
   }
-  ul.appendChild(li);
+  selectedNums.sort(compare);
+  selectedNums.forEach(function (item, idx) {
+    const li = document.createElement("li");
+    li.textContent = item;
+    li.classList.add(colors[Math.ceil(item / 10) - 1]);
+    /*
+  if (item >= 1 && item <= 10) {
+    //1~10
+    //li.classList.add("yellow");
+    li.classList.add(colors[0]);
+  } else if (item >= 11 && item <= 20) {
+    //11~20
+    //li.classList.add("blue");
+    li.classList.add(colors[1]);
+  } else if (item >= 21 && item <= 30) {
+    //21~30
+    //li.classList.add("red");
+    li.classList.add(colors[2]);
+  } else if (item >= 31 && item <= 40) {
+    //li.classList.add("gray");
+    li.classList.add(colors[3]);
+  } else {
+    //li.classList.add("green");
+    li.classList.add(colors[4]);
+  }
+  */
+    ul.appendChild(li);
+  });
+}
+radios.forEach(function (item, idx) {
+  item.addEventListener("change", function () {
+    console.log(idx);
+    result.innerHTML = "";
+    for (let i = 0; i < idx + 1; i++) {
+      makeLotto();
+    }
+  });
 });
 
 function compare(a, b) {
   return a - b;
 }
-//console.log(nums);
-//console.log(selectedNums.sort(compare));
-// const animals = ["cat", "puppy", "tiger"];
-// console.log(animals.splice(0, 1));
-// console.log(animals);
-// console.log(animals[1]);
-// console.log(animals[2]);
