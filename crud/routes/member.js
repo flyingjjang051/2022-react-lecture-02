897c05774da1485066aefa040079bd80e24b56d6
jjang051 <flyjjang051@gmail.com>
2022-12-02 15:45:27 +0900

    env 설정

--- crud/routes/member.js
+++ crud/routes/member.js
@@ -1,6 +1,10 @@
+//몽구스
+
 const express = require("express");
 const router = express.Router();
 const moment = require("moment");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 // db (데이터 베이스)   // 회원가입 만들고 리스트 뿌려보기....
 const subTitle = "회원";
@@ -9,6 +13,25 @@ const gnb = "gnb01";
 
 const memberList = [];
 let memberNo = 0;
+
+mongoose.connect(process.env.MONGO_URL);
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("db연결 성공");
+});
+
+//
+const members = mongoose.Schema({
+  no: "number",
+  id: "string",
+  password: "string",
+  name: "string",
+  phone: "string",
+  address: "string",
+  date: "date",
+});
+
+const Members = mongoose.model("Schema", members);
 
 router.get("/join", (req, res) => {
   res.render("./member/join", { subTitle: subTitle, contentsTitle: "회원가입", className: "member", subVisualNo: subVisualNo, gnb: gnb });
@@ -19,13 +42,36 @@ router.post("/join", (req, res) => {
   const name = req.body.name;
   const phone = req.body.phone;
   const address = req.body.address;
-  memberList.push({ no: ++memberNo, id, password, name, phone, address, date: moment().format("YYYY-MM-DD hh:mm:ss") });
-  res.redirect("/member/list");
+  // memberList.push({ no: ++memberNo, id, password, name, phone, address, date: moment().format("YYYY-MM-DD hh:mm:ss") });
+  //prettier-ignore
+  const newMembers = new Members({ 
+    no: 2, 
+    id: id, 
+    password: password, 
+    name: name, 
+    phone: phone, 
+    address: address, 
+    date: new Date() 
+  });
+  newMembers.save((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      //console.log("멤버 추가 성공");
+      res.redirect("/member/list");
+    }
+  });
 });
 
 router.get("/list", (req, res) => {
-  const list = [...memberList];
-  res.render("./member/list", { subTitle: subTitle, contentsTitle: "회원리스트", className: "member", subVisualNo: subVisualNo, gnb: gnb, list: list });
+  //const list = [...memberList];
+  Members.find((err, members) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("./member/list", { subTitle: subTitle, contentsTitle: "회원리스트", className: "member", subVisualNo: subVisualNo, gnb: gnb, list: members });
+    }
+  });
 });
 
 module.exports = router;
