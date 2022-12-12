2a1562cb7b3fe87fc406ab4d97a638133e15c24e
jjang051 <flyjjang051@gmail.com>
2022-12-12 16:38:41 +0900

    useState

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,15 +1,17 @@
 import React from "react";
-function TodoInsert() {
+function TodoInsert({ insertTodoItem }) {
   function insertTodo() {
     console.log("할일을 추가했습니다.");
   }
+  //props.insertTodoItem();
   return (
     <div className="todo-insert">
       <input type="text" />
-      <button onClick={insertTodo}>
+      <button onClick={insertTodoItem}>
         <i className="fa-solid fa-plus"></i>
       </button>
     </div>
   );
 }
 export default TodoInsert;
+// Hooks
--- todo-app/src/components/TodoMain.js
+++ todo-app/src/components/TodoMain.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import Footer from "./Footer";
 import Header from "./Header";
 import TodoInsert from "./TodoInsert";
@@ -5,17 +7,25 @@ import TodoList from "./TodoList";
 
 function TodoMain() {
   // 리액트에서 제일 힘든거 데이터 전다하기가 힘들다. 그래서 나온게 redux이다.
-  const todoList = [
+  // 리액트는 변수를 바꾸는 걸로 화면을 렌더링하지 않는다.
+  // 컴퍼넌트
+  const initTodoList = [
     { id: 0, done: true, title: "점심먹기" },
     { id: 1, done: false, title: "저녁먹기" },
     { id: 2, done: false, title: "야식먹기" },
     { id: 3, done: true, title: "축구보기" },
-    { id: 4, done: false, title: "리액트 복습하기." },
+    //{ id: 4, done: false, title: "리액트 복습하기." },
   ];
+  // const [상태를 표시하는 변수,상태를 변경하는 함수] = useState();
+  const [todoList, setTodoList] = useState(initTodoList);
+  console.log(...todoList);
+  const insertTodoItem = () => {
+    setTodoList([...todoList, { id: 4, done: false, title: "리액트 복습하기." }]);
+  };
   return (
     <>
       <Header />
-      <TodoInsert />
+      <TodoInsert insertTodoItem={insertTodoItem} />
       <TodoList todos={todoList} title="안녕" msg="메세지" />
       <Footer />
     </>
