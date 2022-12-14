b9378c25a17621241d573c2c63686fc7601ef9ce
jjang051 <flyjjang051@gmail.com>
2022-12-14 12:12:48 +0900

    scss 수정

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -22,17 +22,18 @@ export default function Movie({ title, originalTitle, releaseDate, poster }) {
   );
 }
 */
+// spa  single page application
 export default function Movie({ movieInfo }) {
   return (
     <li>
       <div className="img-box">
         <img src={`https://image.tmdb.org/t/p/w200/${movieInfo.poster_path}`} alt="" />
-        <span>{movieInfo.vote_average}</span>
+        <span className="point">{movieInfo.vote_average}</span>
       </div>
       <div className="info">
         <div className="title-box">
           <h3>{movieInfo.title}</h3>
-          <p className="original-title">{movieInfo.origina_title}</p>
+          <p className="original-title">{movieInfo.original_title}</p>
           <p className="open-date">{movieInfo.release_date}</p>
         </div>
         <div className="overview-box">
