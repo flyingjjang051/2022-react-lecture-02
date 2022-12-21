7a5e7135596e2d4afc9ea183dc7c84beefbee6da
jjang051 <flyjjang051@gmail.com>
2022-12-21 12:10:46 +0900

    diary editor 오나성

---
+++ diary-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import DiaryEditor from "./components/DiaryEditor";
+
+function App() {
+  return (
+    <div className="App">
+      <DiaryEditor></DiaryEditor>
+    </div>
+  );
+}
+
+export default App;
+
